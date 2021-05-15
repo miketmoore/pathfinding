dbc74d0b7d82869902366fea3807effd489e9dfc
Michael Moore <mike@miketmoore.com>
2021-05-15 17:15:51 -0400

    update test to allow for more cases

--- dijkstra_test.go
+++ dijkstra_test.go
@@ -9,57 +9,75 @@ import (
 
 func TestDijkstra(t *testing.T) {
 
-	nodeA := pathfinding.NewSourceNode("a")
-	nodeB := pathfinding.NewNode("b")
-	nodeC := pathfinding.NewNode("c")
-	nodeD := pathfinding.NewNode("d")
-	nodeE := pathfinding.NewNode("e")
-	nodeF := pathfinding.NewDestinationNode("f")
-	//  A
-	//  | \
-	//  |  \
-	//  |   \
-	//  |    \
-	//  1     2
-	//  |     |
-	// [B]   [C]
-	//  |     |
-	//  2     3
-	//  |     |
-	// [D]   [E]
-	//  |     |
-	//  6     5
-	//  |    /
-	//  |   /
-	//  |  /
-	//  | /
-	// [F]
+	tests := []struct {
+		getEdges func() []*pathfinding.Edge
+		expected []string
+	}{
+		{
+			expected: []string{"a", "b", "d", "f"},
+			getEdges: func() []*pathfinding.Edge {
 
-	edges := []*pathfinding.Edge{
-		pathfinding.NewEdge(nodeA, nodeB, 1),
-		pathfinding.NewEdge(nodeA, nodeC, 2),
-		pathfinding.NewEdge(nodeB, nodeD, 2),
-		pathfinding.NewEdge(nodeC, nodeE, 3),
-		pathfinding.NewEdge(nodeD, nodeF, 6),
-		pathfinding.NewEdge(nodeE, nodeF, 5),
+				nodeA := pathfinding.NewSourceNode("a")
+				nodeB := pathfinding.NewNode("b")
+				nodeC := pathfinding.NewNode("c")
+				nodeD := pathfinding.NewNode("d")
+				nodeE := pathfinding.NewNode("e")
+				nodeF := pathfinding.NewDestinationNode("f")
+				//  A
+				//  | \
+				//  |  \
+				//  |   \
+				//  |    \
+				//  1     2
+				//  |     |
+				// [B]   [C]
+				//  |     |
+				//  2     3
+				//  |     |
+				// [D]   [E]
+				//  |     |
+				//  6     5
+				//  |    /
+				//  |   /
+				//  |  /
+				//  | /
+				// [F]
+
+				return []*pathfinding.Edge{
+					pathfinding.NewEdge(nodeA, nodeB, 1),
+					pathfinding.NewEdge(nodeA, nodeC, 2),
+					pathfinding.NewEdge(nodeB, nodeD, 2),
+					pathfinding.NewEdge(nodeC, nodeE, 3),
+					pathfinding.NewEdge(nodeD, nodeF, 6),
+					pathfinding.NewEdge(nodeE, nodeF, 5),
+				}
+			},
+		},
 	}
 
-	shortestPath, err := pathfinding.Dijkstra(edges)
-	if err != nil {
-		fmt.Println(err)
-		t.Errorf("error is unexpected")
+	for _, test := range tests {
+
+		shortestPath, err := pathfinding.Dijkstra(test.getEdges())
+		if err != nil {
+			fmt.Println(err)
+			t.Errorf("error is unexpected")
+		}
+
+		gotLength := len(shortestPath)
+		expectedLength := len(test.expected)
+
+		if gotLength != expectedLength {
+			t.Errorf("length is %d but is expected to be %d", gotLength, expectedLength)
+		}
+
+		for pathIndex, expectedPathValue := range test.expected {
+			if shortestPath[pathIndex].ID != expectedPathValue {
+				t.Errorf("path is invalid [%s]", pathToString(shortestPath))
+			}
+		}
+
 	}
 
-	if len(shortestPath) != 4 {
-		t.Errorf("path length is unexpected: [%d]", len(shortestPath))
-	}
-
-	if shortestPath[0].ID != "a" ||
-		shortestPath[1].ID != "b" ||
-		shortestPath[2].ID != "d" ||
-		shortestPath[3].ID != "f" {
-		t.Errorf("path is invalid [%s]", pathToString(shortestPath))
-	}
 }
 
 func pathToString(path []*pathfinding.Node) string {
