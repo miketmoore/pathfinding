28dddaf5aa2456a7a57b06e4124620e23e4df877
Michael Moore <mike@miketmoore.com>
2021-05-18 10:52:03 -0400

    update example

--- cmd/dijkstra/dijkstra.go
+++ cmd/dijkstra/dijkstra.go
@@ -11,22 +11,24 @@ func main() {
 
 	graph := pathfinding.NewGraph()
 
+	graph.NewSourceNode("0")
+
 	graph.AddEdge("0", "1", 4)
 	graph.AddEdge("1", "2", 8)
-	// graph.AddEdge("2", "3", 7)
-	// graph.AddEdge("3", "4", 9)
-	// graph.AddEdge("4", "5", 10)
-	// graph.AddEdge("5", "6", 2)
-	// graph.AddEdge("6", "7", 1)
-	// graph.AddEdge("7", "8", 7)
+	graph.AddEdge("2", "3", 7)
+	graph.AddEdge("3", "4", 9)
+	graph.AddEdge("4", "5", 10)
+	graph.AddEdge("5", "6", 2)
+	graph.AddEdge("6", "7", 1)
+	graph.AddEdge("7", "8", 7)
 
-	// graph.AddEdge("0", "7", 8)
-	// graph.AddEdge("1", "7", 11)
-	// graph.AddEdge("7", "8", 7)
-	// graph.AddEdge("6", "8", 6)
-	// graph.AddEdge("2", "8", 2)
-	// graph.AddEdge("2", "5", 4)
-	// graph.AddEdge("3", "5", 14)
+	graph.AddEdge("0", "7", 8)
+	graph.AddEdge("1", "7", 11)
+	graph.AddEdge("7", "8", 7)
+	graph.AddEdge("6", "8", 6)
+	graph.AddEdge("2", "8", 2)
+	graph.AddEdge("2", "5", 4)
+	graph.AddEdge("3", "5", 14)
 
 	shortestPathTree, shortestPath, err := pathfinding.Dijkstra(graph)
 	if err != nil {
@@ -34,21 +36,22 @@ func main() {
 		os.Exit(0)
 	}
 
-	for nodeId, node := range shortestPathTree {
-		fmt.Printf("id=%s  distance=%f\n", nodeId, node.TentativeDistance)
-	}
-
 	edges := graph.FindEdgesForNodes(shortestPathTree)
-
+	fmt.Println("---")
+	fmt.Println("edges:")
+	fmt.Println("---")
 	for edgeId, edge := range edges {
 		fmt.Printf("id=%s  distance=%f\n", edgeId, edge.Distance)
 	}
 
-	fmt.Println("shortest path ", shortestPath)
+	fmt.Println("---")
+	fmt.Println("shortest path:")
 	for _, node := range shortestPath {
 		fmt.Printf("%s ", node.ID)
 	}
 	fmt.Println()
-	fmt.Println(graph.GraphVizString())
+	fmt.Println("---")
+
+	fmt.Println(graph.GraphVizString("example"))
 
 }
